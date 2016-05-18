486c55b2cf38dea09c2793de056e81074422085e
Adrián San Segundo <sanse_mostoles@hotmail.com>
2016-05-18 19:45:10 +0200

    SE HA AÑADIDO UN COMBOBOX

--- APP PARA REPOS CLASE/APP PARA REPOS CLASE/Form1.Designer.cs
+++ APP PARA REPOS CLASE/APP PARA REPOS CLASE/Form1.Designer.cs
@@ -30,12 +30,13 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.cbPaises = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(189, 72);
+            this.button1.Location = new System.Drawing.Point(51, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 133);
             this.button1.TabIndex = 0;
@@ -44,7 +45,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(406, 72);
+            this.button2.Location = new System.Drawing.Point(181, 42);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(198, 72);
             this.button2.TabIndex = 1;
@@ -52,11 +53,24 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // cbPaises
+            // 
+            this.cbPaises.FormattingEnabled = true;
+            this.cbPaises.Items.AddRange(new object[] {
+            "España",
+            "Portugal",
+            "Francia"});
+            this.cbPaises.Location = new System.Drawing.Point(182, 153);
+            this.cbPaises.Name = "cbPaises";
+            this.cbPaises.Size = new System.Drawing.Size(197, 21);
+            this.cbPaises.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(651, 473);
+            this.Controls.Add(this.cbPaises);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbPaises;
     }
 }
 
